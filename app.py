dd48fa7dfe3844627788532d23458be2b2db2ca1
Альбина Кобзева <kobzeva.207@gmail.com>
2024-09-27 15:01:09 +0700

    Меню

--- app.py
+++ app.py
@@ -2,20 +2,57 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
 <!doctype html>
 <html>
     <head>
-        <title>Кобзева Альбина Алексеевна, Лабораторная 1</title>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+        <style>
+            a {
+                text-decoration: nonel
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1>Меню (см. ниже)</h>
+        <ul style="font-size: 20px; text-decoration: none">
+            <li><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></li>
+        </ul>
+
+        <footer>
+            &copy; Альбина Кобзева, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Кобзева Альбина Алексеевна, лабораторная 1</title>
     </head>
     <body>
         <header>
             НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h>
-
+        <h1>web-сервер на flask</h1>
+        <p>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </p>
         <footer>
             &copy; Альбина Кобзева, ФБИ-24, 3 курс, 2024
         </footer>
