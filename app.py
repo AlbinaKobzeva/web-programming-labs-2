f261c3c7a5591b515d18a3dd9c69dc393a79b136
Альбина Кобзева <kobzeva.207@gmail.com>
2024-10-24 19:50:48 +0700

    Рефакторинг: выделение второй лабораторной

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
+from lab2 import lab2
+
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
+app.register_blueprint(lab2)
 
 @app.route("/")
 @app.route("/index")
 def start():
     return redirect("/menu", code=302)
+
 
 @app.route('/menu')
 def menu():
@@ -36,124 +40,49 @@ def menu():
 </html>
 '''
 
+@app.errorhandler(404)
+def not_found_404(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
 
-@app.route('/lab2/a')
-def a():
-    return 'без слэша'
+        <h2>Ошибка 404 - такой страницы не существует</h2>
+
+        <footer>
+            &copy; Альбина Кобзева, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+'''
 
 
-@app.route('/lab2/a/')
-def a2():
-    return 'со слэшем'
+@app.errorhandler(500)
+def not_found_500(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
 
-all_flower_list = [
-    {'name': 'роза', 'kolvo': 5},
-    {'name': 'тюльпан', 'kolvo': 15},
-    {'name': 'гипсофила', 'kolvo': 10},
-    {'name': 'ромашка', 'kolvo': 20},
-]
+        <h2>Ошибка 500 - сервер не смог обработать запрос</h2>
 
-@app.route('/lab2/flowers/<int:flower_id>')
-def flowers(flower_id):
-    if flower_id >= len(all_flower_list):
-        return "Такого цветка нет", 404
-    else:
-        return render_template('addflower.html', flower_id=flower_id, flower=all_flower_list[flower_id])
-    
-@app.route('/lab2/add_flower/<name>')
-def add_flower(name):
-    for flower in all_flower_list:
-        if flower['name'] == name:
-            return f"Цветок с именем {name} уже существует.", 400
-    all_flower_list.append({'name': name, 'kolvo': 1})  
-    return redirect(url_for('all_flowers'))
-@app.route('/lab2/flower/')
-def no_flower():
-    return "Вы не задали имя цветка", 400   
-@app.route('/lab2/all_flowers')
-def all_flowers():
-    return render_template('flowers.html', all_flower_list=all_flower_list)
-@app.route('/lab2/flowers/clear')
-def clear_flowers():
-    all_flower_list.clear()
-    return redirect(url_for('all_flowers'))
-
-@app.route('/lab2/example')
-def example ():
-    name = 'Альбина Кобзева'
-    numberLab = '2'
-    groupStudent = 'ФБИ-24'
-    numberCourse = '3 курс'
-    fruits = [
-        {'name': 'яблоки', 'price': 100},
-        {'name': 'груши', 'price': 120},
-        {'name': 'апельсины', 'price': 80},
-        {'name': 'мандарины', 'price': 95},
-        {'name': 'манго', 'price': 321}
-    ]
-    return render_template('example.html', name=name, numberLab=numberLab,
-                           groupStudent=groupStudent, numberCourse=numberCourse, 
-                           fruits=fruits)
-
-@app.route('/lab2/')
-def lab2():
-    return render_template('lab2.html')
-
-@app.route('/lab2/filters')
-def filters():
-    phrase = 'О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных...'
-    return render_template('filter.html', phrase = phrase)
-
-@app.route('/lab2/calc/<int:a>/<int:b>')
-def calc(a, b):
-    return render_template('calc.html', a=a, b=b)
-@app.route('/lab2/calc/')
-def calc_without_numbers():
-    return redirect(url_for('calc', a=1, b=1))
-@app.route('/lab2/calc/<int:a>/')
-def calc_with_a(a):
-    return redirect(url_for('calc', a=a, b=1))
-
-book_list = [
-    {"author": "Хьюберт Селби", "name": "Реквием по мечте", "genre": "Психологический-реализм", "pages": 320},
-    {"author": "Филип Пулман", "name": "Тень горы", "genre": "Фэнтези", "pages": 464},
-    {"author": "Дэниел Киз", "name": "Цветы для Элджернона", "genre": "Научная фантастика", "pages": 311},
-    {"author": "Джордж Оруэлл", "name": "Скотный двор", "genre": "Сатира", "pages": 112},
-    {"author": "Габриэль Гарсиа Маркес", "name": "Любовь во время холеры", "genre": "Роман", "pages": 368},
-    {"author": "Артур Конан Дойл", "name": "Приключения Шерлока Холмса", "genre": "Детектив", "pages": 307},
-    {"author": "Джером Д. Сэлинджер", "name": "Над пропастью во ржи", "genre": "Роман", "pages": 277},
-    {"author": "Джон Стейнбек", "name": "Гроздья гнева", "genre": "Роман", "pages": 464},
-    {"author": "Роберт Гэлбрейт", "name": "Крестный отец", "genre": "Криминальный роман", "pages": 448},
-    {"author": "Эмили Бронте", "name": "Грозовой перевал", "genre": "Роман", "pages": 416},
-    {"author": "Джейн Остин", "name": "Эмма", "genre": "Роман", "pages": 474},
-]
-
-@app.route('/lab2/books')
-def books():
-    return render_template('books.html', book_list=book_list)
-
-objects = [
-    {
-        "name": "Джордан",
-        "image": "джордан.jpg"
-    },
-    {
-        "name": "лакост",
-        "image": "лакост.jpg"
-    },
-    {
-        "name": "баленсиага",
-        "image": "баленсиага.jpg"
-    },
-    {
-        "name": "осирис",
-        "image": "осирис.jpg"
-    },
-    {
-        "name": "рики",
-        "image": "рики.jpg"
-    }
-]
-@app.route('/lab2/spisok')
-def spisok():
-    return render_template('spisok.html', objects=objects)
+        <footer>
+            &copy; Альбина Кобзева, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+'''
---
+++ lab2.py
@@ -0,0 +1,138 @@
+from flask import Blueprint, redirect, url_for, render_template
+lab2 = Blueprint('lab2', __name__)
+
+@lab2.route('/lab2/a')
+def a():
+    return 'без слэша'
+
+
+@lab2.route('/lab2/a/')
+def a2():
+    return 'со слэшем'
+
+all_flower_list = [
+    {'name': 'роза', 'kolvo': 5},
+    {'name': 'тюльпан', 'kolvo': 15},
+    {'name': 'гипсофила', 'kolvo': 10},
+    {'name': 'ромашка', 'kolvo': 20},
+]
+
+@lab2.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(all_flower_list):
+        return "Такого цветка нет", 404
+    else:
+        return render_template('addflower.html', flower_id=flower_id, flower=all_flower_list[flower_id])
+
+
+@lab2.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    for flower in all_flower_list:
+        if flower['name'] == name:
+            return f"Цветок с именем {name} уже существует.", 400
+    all_flower_list.append({'name': name, 'kolvo': 1})  
+    return redirect(url_for('all_flowers'))
+
+
+@lab2.route('/lab2/flower/')
+def no_flower():
+    return "Вы не задали имя цветка", 400
+
+
+@lab2.route('/lab2/all_flowers')
+def all_flowers():
+    return render_template('flowers.html', all_flower_list=all_flower_list)
+
+
+@lab2.route('/lab2/flowers/clear')
+def clear_flowers():
+    all_flower_list.clear()
+    return redirect(url_for('all_flowers'))
+
+
+@lab2.route('/lab2/example')
+def example ():
+    name = 'Альбина Кобзева'
+    numberLab = '2'
+    groupStudent = 'ФБИ-24'
+    numberCourse = '3 курс'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', name=name, numberLab=numberLab,
+                           groupStudent=groupStudent, numberCourse=numberCourse, 
+                           fruits=fruits)
+
+
+@lab2.route('/lab2/')
+def lab_2():
+    return render_template('lab2.html')
+
+
+@lab2.route('/lab2/filters')
+def filters():
+    phrase = 'О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных...'
+    return render_template('filter.html', phrase = phrase)
+
+
+@lab2.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    return render_template('calc.html', a=a, b=b)
+
+
+@lab2.route('/lab2/calc/')
+def calc_without_numbers():
+    return redirect(url_for('calc', a=1, b=1))
+
+
+@lab2.route('/lab2/calc/<int:a>/')
+def calc_with_a(a):
+    return redirect(url_for('calc', a=a, b=1))
+
+book_list = [
+    {"author": "Хьюберт Селби", "name": "Реквием по мечте", "genre": "Психологический-реализм", "pages": 320},
+    {"author": "Филип Пулман", "name": "Тень горы", "genre": "Фэнтези", "pages": 464},
+    {"author": "Дэниел Киз", "name": "Цветы для Элджернона", "genre": "Научная фантастика", "pages": 311},
+    {"author": "Джордж Оруэлл", "name": "Скотный двор", "genre": "Сатира", "pages": 112},
+    {"author": "Габриэль Гарсиа Маркес", "name": "Любовь во время холеры", "genre": "Роман", "pages": 368},
+    {"author": "Артур Конан Дойл", "name": "Приключения Шерлока Холмса", "genre": "Детектив", "pages": 307},
+    {"author": "Джером Д. Сэлинджер", "name": "Над пропастью во ржи", "genre": "Роман", "pages": 277},
+    {"author": "Джон Стейнбек", "name": "Гроздья гнева", "genre": "Роман", "pages": 464},
+    {"author": "Роберт Гэлбрейт", "name": "Крестный отец", "genre": "Криминальный роман", "pages": 448},
+    {"author": "Эмили Бронте", "name": "Грозовой перевал", "genre": "Роман", "pages": 416},
+    {"author": "Джейн Остин", "name": "Эмма", "genre": "Роман", "pages": 474},
+]
+
+@lab2.route('/lab2/books')
+def books():
+    return render_template('books.html', book_list=book_list)
+
+objects = [
+    {
+        "name": "Джордан",
+        "image": "джордан.jpg"
+    },
+    {
+        "name": "лакост",
+        "image": "лакост.jpg"
+    },
+    {
+        "name": "баленсиага",
+        "image": "баленсиага.jpg"
+    },
+    {
+        "name": "осирис",
+        "image": "осирис.jpg"
+    },
+    {
+        "name": "рики",
+        "image": "рики.jpg"
+    }
+]
+@lab2.route('/lab2/spisok')
+def spisok():
+    return render_template('spisok.html', objects=objects)
