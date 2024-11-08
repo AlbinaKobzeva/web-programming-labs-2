998d7923b368a19e6b06b52301e2abd8960603ed
Альбина Кобзева <kobzeva.207@gmail.com>
2024-11-08 14:10:01 +0700

    POST/Redirect/GET

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, redirect
 lab4 = Blueprint('lab4', __name__)
  
 @lab4.route('/lab4/')
@@ -80,3 +80,19 @@ def pow():
         return render_template('lab4/pow.html', error='0 в степени 0 не определено!')
     result = x1 ** x2
     return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+
+
+tree_count = 0
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+    if operation == 'cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count +=1
+    
+    return redirect ('/lab4/tree')
