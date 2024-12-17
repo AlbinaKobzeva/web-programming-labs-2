95213e19973cdc04a096f55cf0a59a05e8f7230c
Альбина Кобзева <kobzeva.207@gmail.com>
2024-12-18 00:33:45 +0700

    Получение информации о фильме/-ах по REST

--- lab7.py
+++ lab7.py
@@ -1,5 +1,45 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, abort
 lab7 = Blueprint('lab7', __name__)
 @lab7.route('/lab7/')
-def main():
+def lab():
     return render_template('lab7/lab7.html')
+films = [
+    {
+        "title": "The Age of Adaline",
+        "title_ru": "Век Адалин",
+        "year": 2015,
+        "description": "Вечно молодая Адалин встречает мужчину, ради которого готова состариться. Фантастическая драма с Блейк Лайвли."
+    },
+    {
+        "title": "A Man Called Ove",
+        "title_ru": "Вторая жизнь Уве",
+        "year": 2015,
+        "description": "Сварливый вдовец-придира находит смысл жизни с появлением новых соседей. Добрый фильм по шведскому бестселлеру."
+    },
+    {
+        "title": "Whiplash",
+        "title_ru": "Одержимость",
+        "year": 2014,
+        "description": "Юный барабанщик на тернистом пути к величию. Остросюжетная драма Дэмьена Шазелла, отмеченная тремя «Оскарами»."
+    },
+    {
+        "title": "Love Actually",
+        "title_ru": "Реальная любовь",
+        "year": 2003,
+        "description": "Калейдоскоп смешных и трогательных историй о любви. Фильм, без которого уже нельзя представить себе Рождество."
+    },
+    {
+        "title": "Atonement",
+        "title_ru": "Искупление",
+        "year": 2007,
+        "description": "Семейная трагедия и Вторая мировая война разлучают влюбленных. Щемящая драма о предательстве и покаянии." 
+    }
+]
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    return films
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    return films[id] 
